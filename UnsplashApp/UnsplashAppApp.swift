61469482853476362eec3bd6806a5a6345790cfd
Jerem <jpoulain58@gmail.com>
2024-01-23 13:46:21 +0100

    Initial Commit

---
+++ UnsplashApp/UnsplashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashAppApp.swift
+//  UnsplashApp
+//
+//  Created by Jeremy POULAIN on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
